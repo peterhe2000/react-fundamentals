a6bbaebf06a7ba437e73ae9d7e93a475bc635c6f
peterhe <peter.he@sandstone.com.au>
2021-09-05 22:49:08 +1000

    06 extra-1 done. Use useRef hook to get reference of form input

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,16 +4,18 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const usernameInputRef = React.useRef(null)
+
   const handleSubmit = event => {
     event.preventDefault()
-    onSubmitUsername(event.target.elements.usernameInput.value)
+    onSubmitUsername(usernameInputRef.current.value)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input type="text" id="usernameInput" />
+        <input type="text" id="usernameInput" ref={usernameInputRef} />
       </div>
       <button type="submit">Submit</button>
     </form>
